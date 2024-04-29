c953abedc22aa30790a11528977c382d86a4bd65
Stepan Shimigonov <shstepanspb@gmail.com>
2024-04-29 12:39:47 +0300

    solution of hw2

---
+++ hw/hw2/src/main.rs
@@ -0,0 +1,36 @@
+struct _SmartSocket {
+    consumption: i32,
+    description: String,
+}
+
+impl _SmartSocket {
+    fn _new(_consumption: i32, _description: String) -> _SmartSocket {
+        todo!()
+    }
+
+    fn _current_consumption(&self) -> i32 {
+        todo!()
+    }
+
+    fn _switch(&self) {
+        todo!()
+    }
+
+    fn _description(&self) -> String {
+        todo!()
+    }
+}
+
+struct _SmartThermometer;
+
+impl _SmartThermometer {
+    fn _new() -> _SmartThermometer {
+        todo!()
+    }
+
+    fn _current_temp(&self) -> i32 {
+        todo!()
+    }
+}
+
+fn main() {}
